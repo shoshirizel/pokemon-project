85a57541c3f984dc42bb7250fd645ddf5d057bdf
Shoshi <sh0534157975@gmail.com>
2020-11-05 22:41:36 +0200

    Update poke_api.py

--- server/poke_api.py
+++ server/poke_api.py
@@ -6,8 +6,10 @@ def chain(pokemon_name):
     url_ = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
     species = requests.get(url=url_, verify=False).json()
     species_url = species['species']['url'] 
-    evolution_chain_url = requests.get(url=species_url, verify=False).json()['evolution_chain']['url']
-    return requests.get(url=evolution_chain_url, verify=False).json()['chain']
+    evolution_chain_url = requests.get(
+        url=species_url, verify=False).json()['evolution_chain']['url']
+    return requests.get(
+        url=evolution_chain_url, verify=False).json()['chain']
 
 def evolve_to(chain, name):
     while chain['species']['name'] != name:
@@ -15,5 +17,6 @@ def evolve_to(chain, name):
     if not chain.get('evolves_to'):
         return None
     new_name = chain['evolves_to'][0]['species']['name']
-    new_poke = requests.get(f"https://pokeapi.co/api/v2/pokemon/{new_name}", verify=False).json()
+    new_poke = requests.get(
+        f"https://pokeapi.co/api/v2/pokemon/{new_name}", verify=False).json()
     return new_poke
